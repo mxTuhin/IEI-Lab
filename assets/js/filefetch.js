cc767b5a2a141a3fc850037cca6ab2a00dcd6f6a
mxTuHin <tuhinmridha11@gmail.com>
2025-10-21 03:55:32 +0600

    Tutorial Write Up Update

--- assets/js/filefetch.js
+++ assets/js/filefetch.js
@@ -1,4 +1,4 @@
-    fetch('layout/navbar.html')
+    fetch('/layout/navbar.html')
     .then(response => response.text())
     .then(data => {
     document.getElementById('docs-nav').innerHTML = data;
